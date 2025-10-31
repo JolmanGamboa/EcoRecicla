55603863f9975a5b0e35575981b5ec723f6d894f
Lenovo_Pc <miguel381@gmail.com>
2025-10-31 00:17:38 -0500

    primer commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
                 includeGroupByRegex("com\\.android.*")
                 includeGroupByRegex("com\\.google.*")
                 includeGroupByRegex("androidx.*")
+
             }
         }
         mavenCentral()
@@ -21,4 +22,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "EcoRecicla"
 include(":app")
+
  
